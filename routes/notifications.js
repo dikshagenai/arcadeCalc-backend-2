b570a4550fb38b4833ddbcf4d154a1efe95aacde
Deepanshu <deepanshubawaliya@gmail.com>
2024-09-17 16:42:54 +0530

    try - 2

--- routes/notifications.js
+++ routes/notifications.js
@@ -11,7 +11,7 @@ class Notification {
 
     async fetchNotifications() {
         try {
-            const notifications = JSON.parse(fs.readFileSync("./data/Notifications/Notifications.json", "utf8"));
+            const notifications = JSON.parse(fs.readFileSync("./data/Notifications/notifications.json", "utf8"));
             return notifications;
         } catch (error) {
             throw new Error('Failed to fetch notifications');
@@ -21,28 +21,13 @@ class Notification {
 
     async addNotifications(notifications) {
         try {
-            console.log('inside try of function')
-            // let notifications = req.body.data;
-            // let imageUrl = notifications.imageUrl;
-            // let content = notifications.content;
-            // let redirectTo = notifications.redirectTo;
-            // let key = notifications.key;
-            // let time = new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' }); // Convert to IST
+
             notifications["time"] = new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' });
+            var notificationsJSON = await JSON.parse(fs.readFileSync("./data/Notifications/notifications.json", "utf8"));
 
+            var finalOutput = JSON.stringify(Object.assign(notificationsJSON, notifications));
 
-
-            console.log('successfully take out values imageUrl loda lhsan')
-
-
-            var notificationsJSON = await JSON.parse(fs.readFileSync("./data/Notifications/Notifications.json", "utf8"));
-            console.log(typeof notificationsJSON)
-
-            notificationsJSON.unshift(notifications)
-
-            // var finalOutput = JSON.stringify(Object.assign(data, users));
-
-            fs.writeFileSync("./data/notifications/Notifications.json", JSON.stringify(notificationsJSON), 'utf-8', (err) => {
+            fs.writeFileSync("./data/notifications/notifications.json", JSON.stringify(finalOutput), 'utf-8', (err) => {
                 console.log(err);
                 return { status: 402, message: err.message, error: err, success: false };
             });
@@ -61,7 +46,7 @@ class Notification {
     async overwriteNotifications(notifications) {
         try {
             // let notifications = req.body.data;
-            fs.writeFileSync("./data/notifications/Notifications.json", JSON.stringify(notifications, null, 4));
+            fs.writeFileSync("./data/notifications/notifications.json", JSON.stringify(notifications, null, 4));
             return { status: 200, message: "Notifications overwrite successfully!" }
         } catch (error) {
             return { status: 500, message: "Unable to delete notifications file.", error: error.message }
@@ -77,7 +62,7 @@ class Notification {
 // ^ ROUTES ---------------
 router.get("/getNotifications", async (req, res) => {
     try {
-        // var notifications = require("../data/Notifications/Notifications.json")
+        // var notifications = require("../data/Notifications/notifications.json")
         const notifications = await new Notification().fetchNotifications()
         res.status(200).json({ Notifications: notifications })
     } catch (error) {
