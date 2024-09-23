126447c52a6910e67e617764d49f1e12d6590841
Deepanshu <deepanshubawaliya@gmail.com>
2024-09-23 21:46:13 +0530

    fixed synrax

--- buildTime.js
+++ buildTime.js
@@ -1,14 +1,18 @@
 // config.js
-// Hosted on main account
 
-// export const SERVER = 'https://arcadecalc.onrender.com';
-// export const DATABASE = 'https://arcadecalc-backend-2.onrender.com';
+// Hosted on main account
+// const SERVER = 'https://arcadecalc.onrender.com';
+// const DATABASE = 'https://arcadecalc-backend-2.onrender.com';
 
 // Hosted on secondary account
-export const SERVER = 'https://arcadecalc-f7cj.onrender.com';
-export const DATABASE = 'https://arcadecalc-backend-2-secondary.onrender.com';
-
+// const SERVER = 'https://arcadecalc-f7cj.onrender.com';
+// const DATABASE = 'https://arcadecalc-backend-2-secondary.onrender.com';
 
 // Testing Url
-// export const SERVER = 'http://localhost:5000';
-// export const DATABASE = 'http://localhost:5001';
+const SERVER = 'http://localhost:5000';
+const DATABASE = 'http://localhost:5001';
+
+module.exports = {
+    SERVER,
+    DATABASE
+};
