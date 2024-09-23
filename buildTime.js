27a616256f75fd7d79582cb7ac3c091d587347cf
Deepanshu <deepanshubawaliya@gmail.com>
2024-09-23 21:53:24 +0530

    fixed server issue

--- buildTime.js
+++ buildTime.js
@@ -5,12 +5,12 @@
 // const DATABASE = 'https://arcadecalc-backend-2.onrender.com';
 
 // Hosted on secondary account
-// const SERVER = 'https://arcadecalc-f7cj.onrender.com';
-// const DATABASE = 'https://arcadecalc-backend-2-secondary.onrender.com';
+const SERVER = 'https://arcadecalc-f7cj.onrender.com';
+const DATABASE = 'https://arcadecalc-backend-2-secondary.onrender.com';
 
 // Testing Url
-const SERVER = 'http://localhost:5000';
-const DATABASE = 'http://localhost:5001';
+// const SERVER = 'http://localhost:5000';
+// const DATABASE = 'http://localhost:5001';
 
 module.exports = {
     SERVER,
